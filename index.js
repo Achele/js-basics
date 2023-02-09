b7c0d6e735b8bf1b3103778cf367edf8431a7fbf
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-02-09 18:10:47 +0100

    js-basics logic

---
+++ index.js
@@ -0,0 +1,21 @@
+// A function that multiplies two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
+// Call the function to a console to multiply any two numbers of your choice and log the answer to the console.
+console.log(multiply(5, 3));
+
+// Create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence.
+const firstName = "Ojoachele";
+const lastName = "Onuh";
+const track = "Frontend Engineer";
+const skills = "Html, Css and JavaScript";
+const favouriteColor = "Yellow";
+const independence = 1960;
+
+// Use these variables to form a sentence and put it in a variable called about
+const about = `My name is ${firstName} ${lastName}. I am a ${track}, proficient in ${skills}. ${favouriteColor} is my favourite color, and my country Nigeria got her independence october 1st ${independence}`;
+
+// Now log the sentence to the console
+console.log(about);
